ac7ec80c3f0659c5893cfa88755c2deb751492df
Fernando <luisf.mechan04@gmail.com>
2023-08-20 16:42:46 -0500

    prueba

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="VAMOS A MORIR PRACTIMACHT"/>
+    <HelloWorld msg="VAMOS A MORIR PRACTIMACHT con fe"/>
   </div>
 </template>
 
