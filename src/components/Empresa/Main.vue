011fe9cefda83dd03d0dfbe791755031850f262a
Fernando <luisf.mechan04@gmail.com>
2023-09-13 17:04:10 -0500

    cards 3

--- src/components/Empresa/Main.vue
+++ src/components/Empresa/Main.vue
@@ -2,16 +2,32 @@
   <div class="contenedor">
       <div class="d-flex">
          <div class="card  "  style="background:#A93226  ;">
-          hola
+          <span class="s1">25</span>
+          <span class="s2">
+            <div><i class="fa fa-user"></i></div>
+            <div>Usuario</div>
+          </span>
          </div>
          <div class="card" style="background:#5DADE2   ;" >
-          hola
+          <span class="s1">25</span>
+          <span class="s2">
+            <div><i class="fa fa-user"></i></div>
+            <div>Usuario</div>
+          </span>
          </div>
          <div class="card" style="background:#52BE80  ;" >
-          hola
+          <span class="s1">25</span>
+          <span class="s2">
+            <div><i class="fa fa-user"></i></div>
+            <div>Usuario</div>
+          </span>
          </div>
          <div class="card" style="background:#ABB2B9  ;" >
-          hola
+          <span class="s1">25</span>
+          <span class="s2">
+            <div><i class="fa fa-user"></i></div>
+            <div>Usuario</div>
+          </span>
          </div>
       </div>
   </div>
@@ -37,5 +53,19 @@ export default {
     width: 24%;
     height: 200px;
     border: none;
+    
+    padding: 40px 50px;
+    flex-wrap: wrap;
   }
+  .contenedor div .card span{
+  
+    color: white;
+  }
+  .contenedor div .card .s1{
+    font-size: 40px;
+  }
+  .contenedor div .card .s2{
+    font-size: 17px;
+  }
+
 </style>
