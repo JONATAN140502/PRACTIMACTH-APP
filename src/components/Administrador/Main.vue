795f4a741e383ad4a409129502d276c7f1119795
Mauricio <ramirocz150701@hotmail.com>
2023-09-13 17:44:11 -0500

    Modificacion en card de Administrador

--- src/components/Administrador/Main.vue
+++ src/components/Administrador/Main.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="contenedor">
     <div class="d-flex">
-         <div class="card  "  style="background:#BB8FCE  ;">
-          hola
+      <div class="card  "  style="background:#A93226  ;">
+          <span class="s1">10</span>
+          <span class="s2">
+            <div><i class="fas fa-building"></i></div>
+            <div>Empresas registradas</div>
+          </span>
          </div>
-         <div class="card" style="background:#BB8FCE  ;" >
-          hola
+         <div class="card" style="background:#5DADE2   ;" >
+          <span class="s1">11</span>
+          <span class="s2">
+            <div><i class="fas fa-user-graduate"></i></div>
+            <div>Alumnos registrados</div>
+          </span>
          </div>
-         <div class="card" style="background:#BB8FCE  ;" >
-          hola
+         <div class="card" style="background:#52BE80  ;" >
+          <span class="s1">5</span>
+          <span class="s2">
+            <div><i class="fas fa-sitemap"></i></div>
+            <div>Areas registradas</div>
+          </span>
          </div>
-         <div class="card" style="background:#BB8FCE  ;" >
-          hola
+         <div class="card" style="background:#52BE80  ;" >
+          <span class="s1">5</span>
+          <span class="s2">
+            <div><i class="fas fa-layer-group"></i></div>
+            <div>Especialidades</div>
+          </span>
          </div>
     </div>
   </div>
@@ -24,8 +40,31 @@ export default {
 </script>
 
 <style scoped>
-  .contenedor{
+.contenedor{
     font-family: 'Open Sans', sans-serif;
-    padding: 20px 50px;
+    padding: 60px 50px;
+  }
+  .contenedor div{
+    justify-content: space-evenly;
+    flex-wrap: wrap;
+   
+  }
+  .contenedor div .card{
+    width: 24%;
+    height: 200px;
+    border: none;
+    
+    padding: 40px 50px;
+    flex-wrap: wrap;
+  }
+  .contenedor div .card span{
+  
+    color: white;
+  }
+  .contenedor div .card .s1{
+    font-size: 40px;
+  }
+  .contenedor div .card .s2{
+    font-size: 17px;
   }
 </style>
