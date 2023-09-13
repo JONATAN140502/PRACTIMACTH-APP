45f24d532efafac727d2288b27784a4c7f3fb24d
GloriaNLA <glorianoelia2000@gmail.com>
2023-09-13 05:54:20 -0500

    publicar practicas

--- src/components/Empresa/Oferta.vue
+++ src/components/Empresa/Oferta.vue
@@ -1,206 +1,239 @@
 <template>
-  <div class="contenedor">
-    <h4>Publicaciones de la Empresa</h4>
-    <div class="contenedor-card d-flex">
-        <div class="contenedor-card1">
-            <div class="card contenedor-card1-texto">
-                <div class="d-flex">  
-                    <div>
-                        <h3>Empresa Soft</h3>
-                        <p>20484850632</p>
+    <div class="contenedor">
+        <h4>Tus publicaciones</h4>
+        <div class="contenedor-card d-flex">
+            <div class="contenedor-card1">
+                <div class="card contenedor-card1-texto">
+                    <div class="d-flex">
+                        <div>
+                            <h3>{{ user }}</h3>
+                            <p>{{ ruc }}</p>
+                        </div>
+                        <div>
+                            <img src="../../assets/empresa.png" alt="">
+                        </div>
                     </div>
-                    <div>
-                        <img src="../../assets/empresa.png" alt="">
-                    </div>
+                </div>
+                <div class="card contenedor-card1-botones ">
+                    <button @click="cambiopagina('publicacion')">Nueva Publicacion</button>
+                    <button @click="cambiopagina('perfil')">Perfil</button>
+                    <button @click="cambiopagina('match')">Mis Match</button>
+
                 </div>
             </div>
-            <div class="card contenedor-card1-botones ">
-                <button @click="cambiopagina('publicacion')">Nueva Publicacion</button>
-                <button @click="cambiopagina('perfil')">Perfil</button>
-                <button @click="cambiopagina('match')">Mis Match</button>
-              
-            </div>
-        </div>
-        <div class="card contenedor-card2">
-            <h5>Ofertas Publicadas</h5>
-            <div class="texto">
-                <div > 
+            <div class="card contenedor-card2">
+                <h5>Ofertas de practicas Publicadas</h5>
+                <div class="texto" v-for="item in dataPractices">
                     <div>
-                        <h5>Analista Programador RPA, Inteligencia Artificial con Node JS y Python</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Perspiciatis perferendis aut exercitationem maiores cumque. 
-                            Iusto ea numquam, reprehenderit ad quia laboriosam sapiente
-                            dolor minus alias, libero soluta, voluptatum magni nam!</p>
+                        <div>
+                            <h5>{{ item.name }}</h5>
+                            <p>{{ item.descripcion }}</p>
+                        </div>
+                        <p>{{ item.date }}</p>
+
                     </div>
-                    <p>2023-09-08</p>
+                    <div class="d-flex">
+                        <img src="../../assets/work.png" alt="">
+                        <div class="">
+                            <p>{{ item.modalidad }}</p>
+                            <p>Vacantes: {{ item.vacant }}</p>
+                            <button type="button" class="btn btn-warning btn-icon mx-1"><i class="fas fa-edit"></i></button>
+                            <button type="button" class="btn btn-danger btn-icon" @click="mtdBeforeDelete(item.id)"><i
+                                    class="fas fa-trash-alt"></i></button>
+                            <!--<button class="match">Ver Match</button>-->
+                        </div>
+                    </div>
 
                 </div>
-                <div class="d-flex">
-                    <img src="../../assets/work.png" alt="">
-                    <div class="">
-                        <p>Modalidad: Presencial </p>
-                        <p>Vacantes: 10</p>
-                        <button  type="button" class="btn btn-danger btn-icon "><i class="fas fa-trash-alt"></i></button>
-                        <button type="button" class="btn btn-warning btn-icon mx-1"><i class="fas fa-edit"></i></button>
-                        <button class="match">Hacer Match</button>
-                    </div>
-                </div>
-
             </div>
-            <div class="texto">
-                <div > 
-                    <div>
-                        <h5>Analista Programador RPA, Inteligencia Artificial con Node JS y Python</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Perspiciatis perferendis aut exercitationem maiores cumque. 
-                            Iusto ea numquam, reprehenderit ad quia laboriosam sapiente
-                            dolor minus alias, libero soluta, voluptatum magni nam!</p>
-                    </div>
-                    <p>2023-09-08</p>
-
-                </div>
-                <div class="d-flex">
-                    <img src="../../assets/work.png" alt="">
-                    <div class="">
-                        <p>Modalidad: Presencial </p>
-                        <p>Vacantes: 10</p>
-                        <button  type="button" class="btn btn-danger btn-icon "><i class="fas fa-trash-alt"></i></button>
-                        <button type="button" class="btn btn-warning btn-icon mx-1"><i class="fas fa-edit"></i></button>
-                        <button class="match">Hacer Match</button>
-                    </div>
-                </div>
-
-            </div>
-            <div class="texto">
-                <div > 
-                    <div>
-                        <h5>Analista Programador RPA, Inteligencia Artificial con Node JS y Python</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Perspiciatis perferendis aut exercitationem maiores cumque. 
-                            Iusto ea numquam, reprehenderit ad quia laboriosam sapiente
-                            dolor minus alias, libero soluta, voluptatum magni nam!</p>
-                    </div>
-                    <p>2023-09-08</p>
-
-                </div>
-                <div class="d-flex">
-                    <img src="../../assets/work.png" alt="">
-                    <div class="">
-                        <p>Modalidad: Presencial </p>
-                        <p>Vacantes: 10</p>
-                        <button  type="button" class="btn btn-danger btn-icon "><i class="fas fa-trash-alt"></i></button>
-                        <button type="button" class="btn btn-warning btn-icon mx-1"><i class="fas fa-edit"></i></button>
-                        <button class="match">Hacer Match</button>
-                    </div>
-                </div>
-
-            </div>
-            
-           
-            
         </div>
     </div>
-  </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
 export default {
-    data(){
-    return{
-      page:null
+    data() {
+        return {
+            page: null,
+            user: null,
+            ruc: null,
+            dataPractices: [],
+            Practice: {
+                id: "",
+            },
+            modal:"add",
+        }
+    },
+    created() {
+        this.user = this.$store.getters.get__company.name
+        this.ruc = this.$store.getters.get__company.ruc
+        this.mtdGetData();
+    },
+    methods: {
+        ...mapActions(['get', 'post']),
+        cambiopagina(page, type) {
+            this.$emit('cambiopagina', page,type);
+
+        },
+        mtdGetData: function () {
+            this.post({
+                url: this.$store.getters.get__url + "/practice/filter",
+                token: this.$store.getters.get__token,
+                params: this.$store.getters.get__company.id,
+            }).then((response) => {
+                this.dataPractices = response.data;
+                //console.log(response.data);
+            })
+                .catch((errors) => { });
+        },
+        //metodo previo a la eliminación
+        mtdBeforeDelete: function (id) {
+            this.Practice.id = id;
+            console.log(id);
+            Swal.fire({
+                title: "¿Estás seguro?",
+                text: "¡No podrás revertir esto!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Eliminar",
+                width: "400px",
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    this.mtdDeleteData();
+                }
+            });
+        },
+        mtdDeleteData: function () {
+            this.post({
+                url: this.$store.getters.get__url + "/practice/destroy",
+                token: this.$store.getters.get__token,
+                params: this.Practice,
+            })
+                .then((response) => {
+                    // Si la eliminación fue exitosa, mostrar una alerta de éxito.
+                    Swal.fire({
+                        title: "¡Eliminado!",
+                        text: "El registro ha sido eliminado.",
+                        icon: "success",
+                        width: "400px",
+                        confirmButtonColor: "rgb(170, 2, 95)",
+                    }).then(() => {
+                        this.mtdGetData();
+                    });
+                })
+                .catch((errors) => {
+                    Swal.fire(
+                        "¡Error!",
+                        "Hubo un problema al eliminar el registro.",
+                        "error"
+                    );
+                });
+        },
+
     }
-   },
-   methods:{
-       cambiopagina(page){
-        this.$emit('cambiopagina',page)
-       
-       }
-   }
 }
 </script>
 
 <style scoped>
- .contenedor{
+.contenedor {
     font-family: 'Open Sans', sans-serif;
     padding: 25px 40px;
- }
- .contenedor h4{
+}
+
+.contenedor h4 {
     color: #1F618D;
     border-bottom: 1px solid #1F618D;
     padding-bottom: 15px;
     margin-bottom: 30px;
-  }
- .contenedor-card{
+}
+
+.contenedor-card {
     justify-content: space-between;
     flex-wrap: wrap;
- }
- .contenedor-card1{
+}
+
+.contenedor-card1 {
     width: 40%;
 
- }
- .contenedor-card1-texto{
-    padding:20px 30px;
-    margin-bottom: 10px;
- 
- }
- .contenedor-card1-texto .d-flex{
-    justify-content: space-between;
-   flex-wrap: wrap;
-  
- }
- 
+}
 
- .contenedor-card2{
+.contenedor-card1-texto {
+    padding: 20px 30px;
+    margin-bottom: 10px;
+
+}
+
+.contenedor-card1-texto .d-flex {
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+}
+
+
+.contenedor-card2 {
     width: 59%;
     padding: 20px 30px;
- }
- .contenedor-card1-botones button{
+}
+
+.contenedor-card1-botones button {
     border: none;
     text-align: left;
     padding: 10px 20px;
     background: white;
- }
- .contenedor-card1-botones button:hover{
-    background:#F2F3F4 ;
- }
- .contenedor-card2 .texto{
+}
+
+.contenedor-card1-botones button:hover {
+    background: #F2F3F4;
+}
+
+.contenedor-card2 .texto {
     justify-content: space-between;
     border-top: 0.5px solid #000000;
     padding-top: 15px;
     flex-wrap: wrap;
     padding-bottom: 15px;
- }
- .contenedor-card2 h5{
+}
+
+.contenedor-card2 h5 {
     margin-bottom: 15px;
- }
- .contenedor-card2 .d-flex{
+}
+
+.contenedor-card2 .d-flex {
     justify-content: space-between;
- }
- .contenedor-card2 .d-flex p{
+}
+
+.contenedor-card2 .d-flex p {
     margin: 5px;
     font-size: 14px;
     font-weight: 400;
- }
- .contenedor-card2 .d-flex img{
+}
+
+.contenedor-card2 .d-flex img {
     width: 90px;
- }
- .contenedor-card2 .d-flex .match{
-    border:none;
-    background:red ;
+}
+
+.contenedor-card2 .d-flex .match {
+    border: none;
+    background: red;
     width: 150px;
-    color:white;
+    color: white;
     border-radius: 5px;
     padding: 7px 0px;
- }
- .contenedor-card2 .d-flex .match:hover{
-    background:#C0392B ;
- }
+}
 
- .contenedor-card2 p{
+.contenedor-card2 .d-flex .match:hover {
+    background: #C0392B;
+}
+
+.contenedor-card2 p {
     text-align: justify;
     font-weight: 400;
- }
- .texto p{
+}
+
+.texto p {
     font-size: 14px;
- }
+}
 </style>
--- src/components/Empresa/Publicaciones.vue
+++ src/components/Empresa/Publicaciones.vue
@@ -1,160 +1,324 @@
 <template>
-    <div class="contenedor">
-        <h4>Nueva Publicacion</h4>
-        
-        <div class="contenedor-card">
-           <div class="card card-1">
-             <h5>Crear Publicacion</h5>
-             <div class="card-input mb-5">
-                <div  class="correo" >
-                    <label for="">Nombre de la publicacion:</label>
-                    <input class="form-control"  type=" text">
-                  </div>
-               <div class="d-flex input-flex" >
-                  <div>
-                    <label for="">Modalidad</label>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Seleccione...</option>
-                        <option value="1">Remoto</option>
-                        <option value="2">Presencial</option>
-                    </select>
-                  </div>
-                  <div>
-                    <label for="">Vacantes :</label>
-                    <input class="form-control"  type=" text">
-                </div>
-               </div>
-              
-               <div class="correo">
-                  <label for="">Descripcion:</label>
-                  <input class="form-control"  type=" text">
-               </div>
-             </div>
-            
-            <div class="card-boton">
-              <button>Crear Publicacion</button>
+  <div class="contenedor">
+    <h4>Nueva Oportunidad de prácticas</h4>
+
+    <div class="contenedor-card">
+      <div class="card card-1">
+        <h5>Crear Publicacion</h5>
+        <div class="card-input mb-5">
+          <div class="correo">
+            <label for="name">Nombre de la publicacion:</label>
+            <input class="form-control" v-model="Practice.name" name="name" id="name" type=" text" />
+          </div>
+          <div class="d-flex input-flex">
+            <div>
+              <label for="modalidad">Modalidad</label>
+              <select class="form-select" name="modalidad" id="modalidad" aria-label="Default select example"
+                v-model="Practice.modalidad">
+                <option disabled value="">Seleccione ...</option>
+                <option value="remoto">Remoto</option>
+                <option value="presencial">Presencial</option>
+              </select>
             </div>
-           </div>
-           <div class="card card-2 text-center">
-             <div class=" ">
-                <img src="../../assets/empresa.png"  class="mx-auto mb-3 " alt="">
-                <h5 class="text-white mb-2">Emppresa Soft</h5>
-                <p class="text-white">empresasoft@gmail.com</p>
-             </div>
-           </div>
-  
+            <div>
+              <label for="workload">Carga de trabajo</label>
+              <select class="form-select" name="workload" id="workload" aria-label="Default select example"
+                v-model="Practice.workload">
+                <option disabled value="">Seleccione ...</option>
+                <option value="Medio tiempo">Medio tiempo</option>
+                <option value="Por horas">Por horas</option>
+              </select>
+            </div>
+            <div>
+              <label for="vacant">Vacantes :</label>
+              <input class="form-control" name="vacant" id="vacant" v-model="Practice.vacant" type=" text" />
+            </div>
+          </div>
+
+          <div class="correo">
+            <label for="descripcion">Descripcion:</label>
+            <textarea class="form-control" rows="3" v-model="Practice.descripcion" name="descripcion" id="descripcion"
+              placeholder="Agrega una breve descripcion de la practica"></textarea>
+          </div>
+          <hr />
+          <h5>Palabras claves</h5>
+          <div class="d-flex input-flex">
+            <div>
+              <label for="areas">Area</label>
+              <select class="form-select" name="areas" id="areas" aria-label="Default select example"
+                v-model="filter.area" @change="mtdSelectArea">
+                <option disabled selected value="">Seleccione...</option>
+                <option v-for="(item, index) in areas" :key="index" :value="item.id">
+                  {{ item.name }}
+                </option>
+              </select>
+            </div>
+            <div>
+              <label for="areas">Especialidad</label>
+              <select class="form-select" name="areas" id="areas" aria-label="Default select example"
+                v-model="filter.specialty" @change="mtdSelectSpecialty">
+                <option disabled selected value="">Seleccione ...</option>
+                <option v-for="(item, index) in specialties" :key="index" :value="item.id">
+                  {{ item.name }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <div class="d-flex input-flex">
+            <div>
+              <label for="subSpecialty">Sub Especialidad</label>
+              <select class="form-select" name="subSpecialty" id="subSpecialty" aria-label="Default select example"
+                v-model="filter.subspecialty" @change="mtdSelectSubspecialty">
+                <option disabled selected value="">Seleccione ...</option>
+                <option v-for="(item, index) in subspecialties" :key="index" :value="item.id">
+                  {{ item.name }}
+                </option>
+              </select>
+            </div>
+          </div>
+          <!--Palabras claves-->
+          <div>
+            <label>Palabras claves</label> <br>
+            <div class="form-check form-check-inline" v-for="(item,index) in knowledges">
+              <input class="form-check-input" type="checkbox"  v-model="Practice.knowledges" v-bind:value="item.id" :key="index" />
+              <label class="form-check-label" >{{item.name}}</label>
+            </div>
+          </div>
         </div>
-        
+
+        <div class="card-boton">
+          <button @click="mtdInsertData">Crear Publicacion</button>
+        </div>
+      </div>
+      <div class="card card-2 text-center">
+        <div class=" ">
+          <img src="../../assets/empresa.png" class="mx-auto mb-3" alt="" />
+          <h5 class="text-white mb-2">{{ user }}</h5>
+          <p class="text-white">{{ correo }}</p>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-  
-  }
-  </script>
-  
-  <style scoped>
-  .contenedor{
-      font-family: 'Open Sans', sans-serif;
-      padding: 25px 40px;
-    }
-    .contenedor h4{
-      color: #1F618D;
-      border-bottom: 1px solid #1F618D;
-      padding-bottom: 15px;
-      margin-bottom: 30px;
-    }
-  
-    .contenedor-card{
-      width: 93%;
-      margin: 0 auto;
-      display: flex;
-      justify-content: space-between;
-    }
-    .card-1{
-      padding: 30px 40px;
-    }
-    .card-1{
-      width: 65%;
-    }
-    .card-1 h5{
-      margin-bottom: 25px;
-      margin-top: 25px;
-      
-    }
-    
-    .input-flex{
-      justify-content: space-between;
-      flex-wrap: wrap;
-      margin-top: 25px;
-    }
-    .input-flex div{
-      width: 49%;
-      margin-bottom: 20px;
-    }
-    .input-flex label{
-      display: block;
-      width: 100%;
-      margin-bottom: 10px;
-    }
-    .input-flex input{
-      width: 100%;
-     
-    }
-   
-    .correo{
-      margin-bottom: 10px;
-    }
-    .correo label{
-      display: block;
-      width: 100%;
-      margin-bottom: 10px;
-    }
-    .correo input{
-      width: 100%;
-    }
-   
-  
-    .card-1 p{
-      font-weight: 300;
-      font-size: 15px;
-     text-align: justify;
-     margin-bottom: 20px;
-    }
-   
-   
-    .card-boton{
-      display: flex; 
-      justify-content: flex-end;
-    
-    }
-    .card-boton button{
-      background: #1F618D ;
-      color: #FFFFFF;
-      border: none;
-      width: 200px;
-      height: 40px;
-      border-radius: 10px;
-    }
-    .card-boton button:hover{
-      background: rgb(31, 97, 141, 0.9) ;
-     
-    }
-    .card-2{
-      width: 33%;
-      height: 400px;
-     
-    }
-    .card-2 div{
-      height: 300px;
-      background: #1F618D;
-      padding: 50px;
-    }
-    .card-2 div img{
-      height: 100px;
-    }
-  
-    
-  
-  </style>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  data() {
+    return {
+      page: null,
+      user: null,
+      ruc: null,
+      areas: [],
+      specialties: [],
+      subspecialties: [],
+      knowledges:[],
+      Practice: {
+        id: "",
+        name: "",
+        modalidad: "",
+        vacant: "",
+        descripcion: "",
+        workload: "",
+        id_company: this.$store.getters.get__company.id,
+        knowledges: [],
+      },
+      Company: {
+        id: this.$store.getters.get__company.id,
+      },
+      filter: {
+        area: null,
+        specialty: null,
+        subspecialty: null,
+      },
+      //radioConocimientos: [],
+    };
+  },
+  created() {
+    this.user = this.$store.getters.get__company.name;
+    this.ruc = this.$store.getters.get__company.ruc;
+    this.correo = this.$store.getters.get__company.correo;
+    this.mtdFilterArea();
+  },
+  methods: {
+    ...mapActions(["get", "post"]),
+    cambiopagina(page) {
+      this.$emit("cambiopagina", page);
+    },
+    mtdInsertData: function () {
+      this.post({
+        url: this.$store.getters.get__url + "/practice/store",
+        token: this.$store.getters.get__token,
+        params: this.Practice,
+      })
+        .then((response) => {
+          if (response.state == 0) {
+            /** todo correcto **/
+            Swal.fire({
+              title: "Registro Exitoso",
+              text: "Perfecto!",
+              icon: "success",
+              //showConfirmButton: true,
+              width: "400px",
+              //padding: '50px 0',
+              //timer: 2000
+              confirmButtonColor: "rgb(170, 2, 95)",
+            }).then(() => {
+              //enviar a la lista de ofertas
+            //var page='oferta';
+            this.$emit('cambiopagina', "oferta");
+            });
+            
+          } else {
+            alert("ocurrió un error");
+          }
+        })
+        .catch((errors) => { });
+    },
+    mtdSelectArea: function () {
+      this.areas.forEach((element, index) => {
+        if (element.id == this.filter.area) {
+          this.specialties = element.specialty;
+          this.subspecialties = [];
+          //console.log(this.specialties);
+        }
+      });
+    },
+    mtdSelectSpecialty: function () {
+      this.specialties.forEach((element, index) => {
+        if (element.id == this.filter.specialty) {
+          this.subspecialties = element.subspecialty;
+        }
+      });
+    },
+    mtdSelectSubspecialty: function () {
+      this.subspecialties.forEach((element, index) => {
+        if (element.id == this.filter.subspecialty) {
+          this.knowledges = element.knowledge;
+        }
+      });
+    },
+    mtdFilterArea: function () {
+      this.post({
+        url: this.$store.getters.get__url + "/company/filterAreas",
+        token: this.$store.getters.get__token,
+        params: this.Company,
+      })
+        .then((response) => {
+          this.areas = response.data;
+          //console.log(this.areas);
+        })
+        .catch((errors) => { });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.contenedor {
+  font-family: "Open Sans", sans-serif;
+  padding: 25px 40px;
+}
+
+.contenedor h4 {
+  color: #1f618d;
+  border-bottom: 1px solid #1f618d;
+  padding-bottom: 15px;
+  margin-bottom: 30px;
+}
+
+.contenedor-card {
+  width: 93%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+}
+
+.card-1 {
+  padding: 30px 40px;
+}
+
+.card-1 {
+  width: 65%;
+}
+
+.card-1 h5 {
+  margin-bottom: 25px;
+  margin-top: 25px;
+}
+
+.input-flex {
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin-top: 25px;
+}
+
+.input-flex div {
+  width: 49%;
+  margin-bottom: 20px;
+}
+
+.input-flex label {
+  display: block;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.input-flex input {
+  width: 100%;
+}
+
+.correo {
+  margin-bottom: 10px;
+}
+
+.correo label {
+  display: block;
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.correo input {
+  width: 100%;
+}
+
+.card-1 p {
+  font-weight: 300;
+  font-size: 15px;
+  text-align: justify;
+  margin-bottom: 20px;
+}
+
+.card-boton {
+  display: flex;
+  justify-content: flex-end;
+}
+
+.card-boton button {
+  background: #1f618d;
+  color: #ffffff;
+  border: none;
+  width: 200px;
+  height: 40px;
+  border-radius: 10px;
+}
+
+.card-boton button:hover {
+  background: rgb(31, 97, 141, 0.9);
+}
+
+.card-2 {
+  width: 33%;
+  height: 400px;
+}
+
+.card-2 div {
+  height: 300px;
+  background: #1f618d;
+  padding: 50px;
+}
+
+.card-2 div img {
+  height: 100px;
+}
+</style>
