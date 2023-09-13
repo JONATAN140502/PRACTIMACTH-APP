888e716e4a1648a03a28eff98debe8866cce26a3
Fernando <luisf.mechan04@gmail.com>
2023-09-13 12:31:35 -0500

    cambio oferta

--- src/components/Estudiante/Oferta.vue
+++ src/components/Estudiante/Oferta.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="contenedor">
     <h4>Publicaciones de la Empresa</h4>
-    <div class="contenedor-card d-flex">
-        <div class="contenedor-card1">
+    <div class="contenedor-card d-flex" >
+        <div class="contenedor-card1" v-if="!mostrarMatch">
             <div class="card contenedor-card1-texto">
                 <div class="d-flex">  
                     <div>
@@ -20,54 +20,55 @@
               
             </div>
         </div>
-        <div class="card contenedor-card2">
+        
+        <div class="card contenedor-card2" :class="{ 'reducido': mostrarMatch }">
             <h5>Ofertas que encajan con tu Perfil</h5>
             <div class="texto">
                 <div > 
                     <div>
+                        <h3>Empresa Soft</h3>
                         <h5>Analista Programador RPA, Inteligencia Artificial con Node JS y Python</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Perspiciatis perferendis aut exercitationem maiores cumque. 
-                            Iusto ea numquam, reprehenderit ad quia laboriosam sapiente
-                            dolor minus alias, libero soluta, voluptatum magni nam!</p>
+                        <p>Modalidad: Presencial </p>
                     </div>
                     <p>2023-09-08</p>
 
                 </div>
                 <div class="d-flex">
-                    <img src="../../assets/work.png" alt="">
+                   
                     <div class="">
-                        <p>Modalidad: Presencial </p>
-                        <p>Vacantes: 10</p>
-                        <button>Ver informacion</button>
-                    </div>
-                </div>
-
-            </div>
-            <div class="texto">
-                <div > 
-                    <div>
-                        <h5>Analista Programador RPA, Inteligencia Artificial con Node JS y Python</h5>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                            Perspiciatis perferendis aut exercitationem maiores cumque. 
-                            Iusto ea numquam, reprehenderit ad quia laboriosam sapiente
-                            dolor minus alias, libero soluta, voluptatum magni nam!</p>
-                    </div>
-                    <p>2023-09-08</p>
-
-                </div>
-                <div class="d-flex">
-                    <img src="../../assets/work.png" alt="">
-                    <div class="">
-                        <p>Modalidad: Presencial </p>
-                        <p>Vacantes: 10</p>
-                        <button>Hacer Match</button>
+                        <button @click="mtdListOferta()">Ver informacion</button>
                     </div>
                 </div>
 
             </div>
            
+           
             
+        </div>
+        <div class=" card contenedor-card3" v-if="mostrarMatch">
+            <h5>Informacion</h5>
+            <div class="texto">
+                <div > 
+                    <div>
+                        <h3>Empresa Soft</h3>
+                        <h5>Analista Programador RPA, Inteligencia Artificial con Node JS y Python</h5>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Perspiciatis perferendis aut exercitationem maiores cumque. 
+                            Iusto ea numquam, reprehenderit ad quia laboriosam sapiente
+                            dolor minus alias, libero soluta, voluptatum magni nam!</p>
+                    </div>
+                    <p>2023-09-08</p>
+
+                </div>
+                <div class="d-flex">
+                    <img src="../../assets/work.png" alt="">
+                    <div class="">
+                        <p>Modalidad: Presencial </p>
+                        <p>Vacantes: 10</p>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
   </div>
@@ -77,6 +78,8 @@
 export default {
     data(){
     return{
+      firstClick: true,
+      mostrarMatch: false,
       page:null
     }
    },
@@ -84,6 +87,11 @@ export default {
        cambiopagina(page){
         this.$emit('cambiopagina',page)
        
+       },
+       mtdListOferta:function(){
+        if (this.firstClick) {
+          this.mostrarMatch = !this.mostrarMatch;
+          this.firstClick = false;}
        }
    }
 }
@@ -122,6 +130,10 @@ export default {
 
  .contenedor-card2{
     width: 59%;
+    padding: 20px 30px;
+ }
+ .contenedor-card3{
+    width: 50%;
     padding: 20px 30px;
  }
  .contenedor-card1-botones button{
@@ -166,6 +178,51 @@ export default {
  }
 
  .contenedor-card2 p{
+    text-align: justify;
+    font-weight: 400;
+ }
+ .reducido{
+    width: 49%;
+ }
+
+ 
+
+
+
+
+ .contenedor-card3 .texto{
+    justify-content: space-between;
+    border-top: 0.5px solid #000000;
+    padding-top: 15px;
+    flex-wrap: wrap;
+    padding-bottom: 15px;
+ }
+ .contenedor-card3 h5{
+    margin-bottom: 15px;
+ }
+ .contenedor-card3 .d-flex{
+    justify-content: space-between;
+ }
+ .contenedor-card3 .d-flex p{
+    margin: 5px;
+    font-size: 14px;
+    font-weight: 400;
+ }
+ .contenedor-card3 .d-flex img{
+    width: 70px;
+ }
+ .contenedor-card3 .d-flex button{
+    border:none;
+    background:red ;
+    width: 150px;
+    color:white;
+    border-radius: 5px;
+ }
+ .contenedor-card3 .d-flex button:hover{
+    background:#C0392B ;
+ }
+
+ .contenedor-card3 p{
     text-align: justify;
     font-weight: 400;
  }
